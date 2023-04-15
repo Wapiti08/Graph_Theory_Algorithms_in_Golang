ba8de79cab522eea7d87ed084e029033e3c624ce
谭卓然 <newt.tan@dbappsecurity.com.cn>
2023-04-15 16:06:22 +0100

    build node class for adj list

---
+++ Graph Representation/adj_list_node_class.go
@@ -0,0 +1,33 @@
+/*
+ * @Author: Newt Tan
+ * @Date: 2023-04-15 15:57:44 
+ * @Last Modified by:   Newt Tan
+ * @Last Modified time: 2023-04-15 15:57:44 
+ */
+
+package main
+
+import (
+	"fmt"
+	"container/list"
+)
+
+type Node struct {
+	// define the node name
+	name string
+	// define the node neighbors
+	ngbs *list.List
+}
+
+type Graph struct {
+	// define all nodes
+	
+	// define hashmap (string, *node)
+
+}
+
+
+func main{
+
+
+}
