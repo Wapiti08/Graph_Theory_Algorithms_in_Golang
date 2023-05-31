f937459dffe75ad3047542d719d32517f1ddfa01
Wapiti08 <tzrzhuoran@gmail.com>
2023-05-31 11:29:26 +0100

    mini_degree_trio

--- Graph Representation/Mini_Degree_Trio.go
+++ Graph Representation/Mini_Degree_Trio.go
@@ -1,3 +1,54 @@
 package main
 
-func minTrioDegree()
+import "fmt"
+
+func minTrioDegree(n int, roads [][]int) int {
+
+	deg := make([]int, n+1)
+	edges := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		edges[i] = make([]int, n+1)
+	}
+	for i := 0; i < len(roads); i++ {
+		x := roads[i][0]
+		y := roads[i][1]
+
+		deg[x]++
+		deg[y]++
+		edges[x][y]++
+		edges[y][x]++
+	}
+
+	ans := 0
+
+	for i := 1; i < n+1; i++ {
+		for j := i + 1; j <= n; j++ {
+			for k := j + 1; k <= n; k++ {
+				// the minimum degree of a connected trio: minux 6 for edges in cycle
+				if edges[i][j] != 0 && edges[j][k] != 0 && edges[i][k] != 0 {
+					ans = min(deg[i]+deg[j]+deg[k]-6, ans)
+
+				}
+
+			}
+		}
+	}
+	if ans == 0 {
+		return -1
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	n := 6
+	roads := [][]int{{1, 2}, {1, 3}, {3, 2}, {4, 1}, {5, 2}, {3, 6}}
+	result := minTrioDegree(n, roads)
+	fmt.Println(result)
+}
