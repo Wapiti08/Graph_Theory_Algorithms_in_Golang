7cc7bdd12fd15e321aef7a4dc260a9c9eda85072
Wapiti08 <tzrzhuoran@gmail.com>
2023-06-20 08:08:19 +0100

    upload right solution

--- Breadth First Search/snake_ladder_cor.go
+++ Breadth First Search/snake_ladder_cor.go
@@ -37,24 +37,56 @@ func NewGraph(v int) *SocialGraph {
 	return g
 }
 
-func (g *SocialGraph) addedge(snake []Pair, ladder []Pair) {
+func (g *SocialGraph) addedge(i, j int, undir bool) {
+	/*
+		param g: instance of SocialGraph
+		param i: edge start
+		param j: edge end
+		param undir: whether undirected
+	*/
+	// if g.Nbrs[i] == nil {
+	// g.Nbrs[i] = list.New()
+	// }
+	g.Nbrs[i].PushBack(j)
+
+	if undir {
+		// if g.Nbrs[j] == nil {
+		// g.Nbrs[j] = list.New()
+		// }
+		g.Nbrs[j].PushBack(i)
+	}
+
+}
+
+func (g *SocialGraph) min_dict_throws(snakes []Pair, ladders []Pair) {
+	board := make(map[int]int, g.V+1)
+	// define the position change for snake
+	for _, snake := range snakes {
+		s := snake.First
+		e := snake.Second
+		board[s] = e - s
+	}
+	// define the position change for ladder
+	for _, ladder := range ladders {
+		s := ladder.First
+		e := ladder.Second
+		board[s] = e - s
+	}
+
 	// build the graph based on snake and ladder positions
-	for i := 1; i <= g.V-6; i++ {
+	for i := 1; i <= g.V; i++ {
 		// the max value of mice is 6, min value is 1
-		for j := i + 1; j <= i+7; j++ {
+		for mice := 1; mice <= 6; mice++ {
 			// check the snake or ladder and whether dest
-			resp_sna, res_sna := IsElementExist(j, snake)
-			if resp_sna {
-				j = res_sna
-			} else {
-				resp_lad, res_lad := IsElementExist(j, ladder)
-				if resp_lad {
-					j = res_lad
-				}
-				g.Nbrs[i].PushBack(j)
+			v := i + mice
+			v += board[v]
+			if v <= g.V {
+				g.addedge(i, v, false)
 			}
+
 		}
 	}
+
 }
 
 func (g *SocialGraph) PrintEdgeList() {
@@ -69,71 +101,42 @@ func (g *SocialGraph) PrintEdgeList() {
 	}
 }
 
-func (g *SocialGraph) min_dict_throws(source, dest int) {
-	// find the shorted path using breadth first search
+func (g *SocialGraph) bfss(source int, dest int) {
+	// build queue to save source
 	q := list.New()
-	visited := make([]bool, g.V)
+	// build list to save visted node with map <source, bool> format
+	visited := make([]bool, g.V+1)
 
 	q.PushBack(source)
 	visited[source] = true
-	// define the dist from the source
-	dist := make([]int, g.V)
-	parent := make([]int, g.V)
-	parent := make([]int, g.V)
 
-	for i := 0; i < g.V; i++ {
-		parent[i] = -1
-	}
-
+	dist := make([]int, g.V+1)
 	dist[source] = 0
-	// initialize the first element
-	parent[source] = source
 
+	// check whether the q is empty ---- have to use for instead of if
 	for q.Len() > 0 {
 		f := q.Front().Value.(int)
 		q.Remove(q.Front())
-		// traverse the neighbors of f
+		// need to give int type to list element
 		for e := g.Nbrs[f].Front(); e != nil; e = e.Next() {
 			nbr := e.Value.(int)
 			if !visited[nbr] {
 				q.PushBack(nbr)
-				parent[nbr] = f
 				dist[nbr] = dist[f] + 1
 				visited[nbr] = true
 			}
 		}
+
 	}
+	fmt.Println(dist[dest])
 
-	// print out the shorted path through parent for given node
-	if dest != -1 {
-		temp := dest
-		if temp != source {
-			fmt.Printf("%d -- ", temp)
-			temp = parent[temp]
-		}
-		fmt.Printf("%d", source)
-	}
-
-}
-
-func IsElementExist(ele int, tup []Pair) (bool, int) {
-	for _, element := range tup {
-		if ele == element.First {
-			return true, element.Second
-		}
-
-		return false, ele
-	}
-
-	return false, ele
 }
 
 func main() {
 	g := NewGraph(36)
 	ladders := []Pair{{2, 15}, {5, 7}, {9, 27}, {18, 29}, {25, 35}}
 	snakers := []Pair{{17, 4}, {20, 6}, {34, 12}, {24, 16}, {32, 30}}
-	g.addedge(snakers, ladders)
-	g.PrintEdgeList()
-	g.min_dict_throws(1, 36)
+	g.min_dict_throws(snakers, ladders)
+	g.bfss(1, 36)
 
 }
