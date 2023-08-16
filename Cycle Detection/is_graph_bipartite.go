baf1482d935185a3f1ba08804cd12a7b48dffe09
Wapiti08 <tzrzhuoran@gmail.com>
2023-08-16 10:14:49 +0100

    upload solution for graph bipartite

--- Cycle Detection/is_graph_bipartite.go
+++ Cycle Detection/is_graph_bipartite.go
@@ -1,55 +1,93 @@
-import "containers/list"
+// import "container/list"
 
-type Graph struct {
-    V: n
-    Nbrs: []*list.List
-}
+// type Graph struct {
+//     V int
+//     Nbrs []*list.List
+// }
 
-func newGraph(n int) *Graph {
-    g := &Graph{
-        V: n,
-        Nbrs: make([]*list.List,n),
-    }
-    for i:=0; i<n; i++ {
-        g.Nbrs[i] = list.New()
-    }
+// func newGraph(n int) *Graph {
+//     g := &Graph{
+//         V: n,
+//         Nbrs: make([]*list.List, n),
+//     }
+//     for i:=0; i<n; i++ {
+//         g.Nbrs[i] = list.New()
+//     }
 
-    return g
-}
+//     return g
+// }
 
-func addEdge(g *Graph, graph [][]int) {
-    
-}
+// func addEdge(g *Graph, graph [][]int) *Graph {
+//     for row := range graph {
+//         for column := range graph[row] {
+//             g.Nbrs[row].PushBack(graph[row][column])
+//         }
+//     }
+//     return g
+// }
 
-func bfshelper(g *Graph, node int, visited []int, parent []int) {
-    visited[node] = 1
+// func dfshelper(g *Graph, node int, visited []int, parent int, color int) bool {
+//     visited[node] = color
+//     if g.Nbrs[node].Len() != 0 {
+//         for e := g.Nbrs[node].Front(); e!=nil; e=e.Next() {
+//             parent = node
+//             nbr := e.Value.(int)
+//             if visited[nbr]==0 {
+//                 subprob := dfshelper(g, nbr, visited, parent, 3-color)
+//                 if !subprob {
+//                     return false
+//                 }   
+//             } else if nbr != parent && color == visited[nbr] {
+//                 return false
+//             } 
+//         }
+//         return true
+//     } else {
+//         node += 1
+//         if visited[node]==0 {
+//                 subprob := dfshelper(g, node, visited, -1, 3-color)
+//                 if !subprob {
+//                     return false
+//                 }   
+//         }
+//         return true
+//     }
 
-    for e := g.Nbrs[node].Front(); e!=nil; e=e.Next() {
-        nbr := e.Value.(int)
-        if visited[nbr]!=0 {
-            visited[nbr] = 2
-            parent[nbr] = node
-            bfshelper(g, nbr, visited, parent)   
-        } 
-        if visited[nbr] == visisted[node] && nbr !=parent[node] {
-            return false
-        }    
-    
-    }
-    return true
 
-}
+// }
 
 
-// func dfshelper() {
+// func bfshelper() {
 
 // }
 
 
 func isBipartite(graph [][]int) bool {
-    parent := make([]int, g.V)
-    visited := make([]int, g.V)
-    g := NewGraph(len(graph))
+    n := len(graph)
+    colors := make([]int, n)
+    q := make([]int, n)
 
+    for i:=0 ; i<n; i++ {
+        if colors[i] != 0 {
+            continue
+        }
+        colors[i]=1
+        q = append(q, i)
+        // implement bfs
+        for len(q)>0 {
+            temp := q[0]
+            // pop out the first element
+            q = q[1:]
+            for _, nbr := range graph[temp] {
+                if colors[nbr] == 0 {
+                    colors[nbr]=-colors[temp]
+                    q = append(q, nbr)
+                } else if colors[nbr] == colors[temp] {
+                    return false
+                }
+            }
+        }
+    }
+    return true
 
 }
